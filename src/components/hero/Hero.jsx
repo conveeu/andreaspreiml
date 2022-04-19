42f8e78085b873561e1886933715b80137c04922
conveeu <office@conve.eu>
2022-04-19 17:03:01 +0200

    Diverse Änderungen

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ const heroContent = {
   heroTitleName: "Andreas Preiml",
   heroDesignation: "Dein Digi-Coach",
   heroDescriptions: `Wer heute bestehen will, muss wissen was er tut. Nicht nur auf dem eigenen Fachgebiet. Alles wird mehr und mehr digital. Es braucht jemanden von außen. Einen Sparringpartner und Coach auf dem Weg zum digitalen Erfolg. Das bin ich.`,
-  heroBtn: "Coaching-Termin vereinbaren",
+  heroBtn: "Kostenloses Erstgespräch vereinbaren",
 };
 
 const Hero = () => {
